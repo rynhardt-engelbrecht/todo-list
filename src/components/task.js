9059138e5ac26b02967026a9bd38b2b3fc0ba8c3
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2024-04-13 23:05:22 +0200

    feat: implement dynamic ID creation for Task

--- src/components/task.js
+++ src/components/task.js
@@ -1,15 +1,34 @@
 const PubSub = require('pubsub-js');
 
 class Task {
-  constructor(title = '', desc = '', dueDate = new Date(), prio = 1, id = 0) {
-    this.title = title;
-    this.desc = desc;
-    this.dueDate = dueDate;
-    this.prio = prio;
-    this.isChecked = false;
-    this.id = 0;
+  constructor(title = '', desc = '', dueDate = new Date(), prio = 1) {
+    const activeProject = JSON.parse(localStorage.getItem('active-project'));
 
-    PubSub.publish('newTask', this);
+    if (activeProject) {
+      this.title = title;
+      this.desc = desc;
+      this.dueDate = dueDate;
+      this.prio = prio;
+      this.isChecked = false;
+      this.id = this.createID(activeProject);
+
+      PubSub.publish('newTask', this);
+      activeProject.taskList.push(this);
+      PubSub.publish('updateTaskList', activeProject);
+    } else {
+      console.error('task creation failed');
+      return;
+    }
+  }
+
+  createID(activeProject) {
+    const taskList = activeProject.taskList;
+
+    if (taskList.length > 0) {
+      return taskList[taskList.length - 1].id + 1;
+    }
+
+    return 0;
   }
 
   static updateInfo(obj, title, desc, dueDate, prio) {

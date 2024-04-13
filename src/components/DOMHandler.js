bec3b05400cd43115bffcfbd1268f1661f59b252
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2024-04-13 15:39:41 +0200

    feat: implement task addition to DOM
    Implemented the addTaskToDOM function in the DOMHandler object, to be
    able to add a newly create task object to the DOM.
    
    Also added the helper functions, createElement and createInput, to
    streamline the process of creating new HTML elements and specifically
    HTML input elements. This makes the code for creating a new element of
    any type easier to read as it's not a bunch of lines setting the
    attributes of the element in a row, but instead just values passed as
    arguments to a function to create an element.

--- src/components/DOMHandler.js
+++ src/components/DOMHandler.js
@@ -1,12 +1,59 @@
 const PubSub = require('pub-sub.js');
 
-(function initializeDOMHandler() {
+const DOMHandler = (function initializeDOMHandler() {
   return {
-    createTaskElement: function(msg, data) {
+    addTaskToDOM: function(msg, data) {
+      console.log(msg);
 
+      const taskList = document.querySelector('#task-list');
+      const taskItem = createTaskElement(data);
+
+      taskList.appendChild(taskItem);
     },
     subscriptions: [
-      PubSub.subscribe('newTask', this.createTaskElement),
+      PubSub.subscribe('newTask', this.addTaskToDOM),
     ]
   }
+
+  function createTaskElement(data) {
+    const task = createElement('', 'task-item');
+
+    const taskComponents = [
+      createElement(data.title, 'task-title'),
+      createElement(data.desc, 'task-desc'),
+      createElement(data.dueDate, 'task-date'),
+      createElement(data.prio, 'task-prio'),
+      createInput(false, 'task-check', 'checkbox')
+    ];
+
+    taskComponents.forEach(e => {
+      task.appendChild(e);
+    });
+
+    task.setAttribute('data-id', data.id);
+
+    return task;
+  }
+
+  // streamline the creation of a new DOM element
+  function createElement(text = '', name = '', type = 'div') {
+    const element = document.createElement(type);
+
+    element.textContent = text;
+    element.className = name;
+
+    return element;
+  }
+
+  // streamline the creation of a new HTML input element
+  function createInput(value = '', name = '', type = 'text') {
+    const input = createElement('', name, 'input');
+
+    input.value = value;
+    input.type = type;
+
+    return input;
+  }
 })();
+
+export default DOMHandler;

ecce79a68f87a58a6dc8a88e9c0cff09c204abda
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2024-04-13 15:55:55 +0200

    feat: implement createProject
    Added the createProject factory function to allow for creation of
    project objects, which has the todoList attribute to keep track of a
    'task list' of Task objects.

--- src/components/project.js
+++ src/components/project.js
@@ -1,1 +1,13 @@
 const PubSub = require('pubsub-js');
+
+function createProject(title = '', todoList = [], id = 0) {
+  const obj = {
+    title,
+    todoList,
+    id
+  }
+
+  PubSub.publish('newProject', obj);
+
+  return obj;
+}

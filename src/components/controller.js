eeb844e23f734d0707fb03ac3683448195c80542
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2024-04-13 21:16:22 +0200

    feat: implement disabling task items
    Added a way to disabled completed task items by checking the .task-check
    checkbox.

--- src/components/controller.js
+++ src/components/controller.js
@@ -3,6 +3,7 @@ const PubSub = require('pubsub-js');
 const controller = (() => {
 
   const prioritySelectListener = function(msg, data) {
+    console.log(msg);
     const prioritySelect = data.querySelector('.task-prio select');
 
     prioritySelect.addEventListener('change', () => {
@@ -11,9 +12,25 @@ const controller = (() => {
     })
   }
 
+  const taskCheckListener = function(msg, data) {
+    console.log(msg);
+    const checkbox = data.querySelector('.task-check');
+    const prioSelect = data.querySelector('.task-prio select')
+
+    checkbox.addEventListener('change', () => {
+      data.classList.toggle('complete');
+      if (prioSelect.disabled) {
+        prioSelect.disabled = false;
+      } else {
+        prioSelect.disabled = true;
+      }
+    });
+  }
+
   return {
     subscriptions: [
       PubSub.subscribe('newTaskElement', prioritySelectListener),
+      PubSub.subscribe('newTaskElement', taskCheckListener)
     ],
   };
 })();

7f9fafefb7af7d9bbe92466cc5c08d0a13362c51
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2024-04-13 14:54:44 +0200

    feat: implement createTask
    Added the createTask factory function used to create a 'task' object.

--- src/components/task.js
+++ src/components/task.js
@@ -1,1 +1,26 @@
 const PubSub = require('pubsub-js');
+
+function createTask(title = '', desc = '', dueDate = new Date(), prio = 1, isChecked = false) {
+  const obj = {
+    title,
+    desc,
+    dueDate,
+    prio,
+    isChecked,
+    updateInfo: function(title, desc, dueDate, prio) {
+      this.title = title;
+      this.desc = desc;
+      this.dueDate = dueDate;
+      this.prio = prio;
+    },
+    updateChecked: function(checkState) {
+      this.checked = checkState;
+    },
+    subscription: PubSub.subscribe('updateTask', updateInfo)
+  }
+
+  // notify subscribers of the 'newTask' topic
+  PubSub.publish('newTask', obj);
+
+  return obj;
+}

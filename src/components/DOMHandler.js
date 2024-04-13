477f9f2118564ef40dab8fc39edd939abedf5822
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2024-04-13 15:19:02 +0200

    feat: implement initializeDOMHandler
    Added the IIFE initializeDOMHandler, which creates an object containing
    functions to manage the DOM. It's written as an IIFE because only one
    instance will ever be needed.

--- src/components/DOMHandler.js
+++ src/components/DOMHandler.js
@@ -1,1 +1,12 @@
 const PubSub = require('pub-sub.js');
+
+(function initializeDOMHandler() {
+  return {
+    createTaskElement: function(msg, data) {
+
+    },
+    subscriptions: [
+      PubSub.subscribe('newTask', this.createTaskElement),
+    ]
+  }
+})();
